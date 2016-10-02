e50d27a100ca60f4ca2a68aa45a664aad4fb1e9a
pivito <migue_.17@hotmail.com>
2016-10-02 13:52:36 +0200

    Update AppDelegate.swift

--- Semana 4 Proyecto 2/AppDelegate.swift
+++ Semana 4 Proyecto 2/AppDelegate.swift
@@ -8,39 +8,57 @@
 
 import UIKit
 
-@UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
-        return true
+enum Velocidades : Int{
+    case Apagado = 0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadAlta = 120
+    
+    init( velocidadInicial : Velocidades )
+    {
+        self = velocidadInicial
     }
 
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+}
+
+class Auto {
+    
+    var velocidad : Velocidades
+    
+    init()
+    {
+        self.velocidad = .Apagado
     }
+    func cambioDeVelocidad( ) -> ( actual : Int, velocidadEnCadena: String)
+    {
+        var velocidadActual : Int
+        
+        var velocidadEnCadena : String
+        
+        switch velocidad {
+        case .Apagado:
+            velocidadEnCadena = "Apagado"
+            velocidadActual = velocidad.rawValue
+            velocidad = .VelocidadBaja
+        case .VelocidadBaja:
+            velocidadActual = velocidad.rawValue
+            velocidad = .VelocidadMedia
+            velocidadEnCadena = "Velocidad Baja"
+        case .VelocidadMedia:
+            velocidadActual = velocidad.rawValue
+            velocidad = .VelocidadAlta
+            velocidadEnCadena = "Velocidad Media"
+        case .VelocidadAlta:
+            velocidadEnCadena = "Velocidad Alta"
+            velocidadActual = velocidad.rawValue
+            velocidad = .VelocidadMedia
+        }
+        
+        return (velocidadActual, velocidadEnCadena)
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     }
+}
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-    }
+var auto = Auto()
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+for i in 1...20{
+    print(auto.cambioDeVelocidad())
 }
 
