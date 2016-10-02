135b8efc77d3762575a28a167be6d49f132e19e7
pivito <migue_.17@hotmail.com>
2016-10-02 13:37:35 +0200

    Initial Commit

---
+++ Semana 4 Proyecto 2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Semana 4 Proyecto 2
+//
+//  Created by miguel Garcia Sanchez on 2/10/16.
+//  Copyright © 2016 miguel Garcia Sanchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
